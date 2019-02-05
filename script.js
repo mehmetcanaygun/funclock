6cd4d91e6a0997ee55843bf1a30e36d26550f96a
Can <mca199667@gmail.com>
2019-02-05 12:33:35 +0300

    Display time

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 var canvas = document.getElementById("my-canvas");
 var ctx = canvas.getContext("2d");
 
-var puppy1 = new Image();
-puppy1.src = "img/puppy1.jpg";
-var puppy2 = new Image();
-puppy2.src = "img/puppy2.jpg";
-var puppy3 = new Image();
-puppy3.src = "img/puppy3.jpg";
+var puppyImg = new Image();
 
-ctx.fillStyle = "#FF0000";
-ctx.font = "30px Monoton";
-ctx.textAlign = "center";
-canvas.textBaseline = "bottom";
-ctx.fillText("Hello World", canvas.width / 2, canvas.height - 60);
-ctx.drawImage(puppy1, 0, 0);
+var puppyMessage = "Hello World";
+
+var timeWrapper = document.getElementById("time-wrapper");
+
+puppyImg.onload = function() {
+  ctx.drawImage(puppyImg, 0, 0);
+  ctx.fillStyle = "#FF0000";
+  ctx.font = "30px Monoton";
+  ctx.textAlign = "center";
+  canvas.textBaseline = "bottom";
+  ctx.fillText(puppyMessage, canvas.width / 2, canvas.height - 60);
+};
+puppyImg.src = "img/puppy1.jpg";
+
+window.setInterval(function() {
+  /// call your function here
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+
+  timeWrapper.innerHTML = hour + ":" + minute + ":" + second;
+}, 1000);
